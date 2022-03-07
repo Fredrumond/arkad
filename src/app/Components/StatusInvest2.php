a9b70dedc4cdce1cf5672bef6a88819862091db4
Frederico <fredrumond@gmail.com>
2022-03-07 20:26:18 -0300

    config test coverage

--- src/app/Components/StatusInvest2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Components;
-
-class StatusInvest {
-    public function currentPrice($value){
-        // return Util::removeSpaceCaracter(explode("R$",explode("arrow",$value)[0])[1]);
-    }
-}
