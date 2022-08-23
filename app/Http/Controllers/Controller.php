54bbfc5dd02ee00a4834a11117c30c5e2eb33976
Frederico Drumond <frederico.drumond@playkids.com>
2022-08-23 20:57:32 -0300

    feat: create funds routes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,12 +7,20 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
 /**
- * @OA\Info(
- *    title="ARKED - API",
- *    version="1.0.0",
+ * @OA\OpenApi(
+ *   @OA\Server(
+ *      url="/api/v1"
+ *   ),
+ *   @OA\Info(
+ *      title="ARKAD - API",
+ *      version="1.0.0",
+ *   ),
  * )
  */
+
+
 
 class Controller extends BaseController
 {
--- app/Http/Controllers/FundsController.php
+++ app/Http/Controllers/FundsController.php
@@ -8,6 +8,26 @@ use App\Services\ArkadCrawlerService;
 
 class FundsController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/actives/funds",
+     *     tags={"Ativos"},
+     *     summary="Retorna lista de fundos imobiliarios",
+     *     description="Utilize este método para listar os fundos imobiliarios cadastrado na base.
+                       Leia a documentação sobre paginação e filtros de busca.",
+     *
+     *      @OA\Parameter(
+     *          name="codes",
+     *          description="Atributa para listar os ativos para consulta",
+     *          in="query",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *     ),
+     *     @OA\Response(response="200", description="Consulta realizada com sucesso.")
+     * )
+     */
+
     public function index(Request $request)
     {
         $searchQuerey = $request->query('codes');
