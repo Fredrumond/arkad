726ccd5a2cb0508e366c3a89deec8fdccbaef098
Frederico Drumond <frederico.drumond@playkids.com>
2022-08-23 21:07:15 -0300

    feat: create actions route

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('actives')->group(function () {
         Route::get('/funds/', [App\Http\Controllers\FundsController::class, 'index']);
         Route::get('/funds/{code}', [App\Http\Controllers\FundsController::class, 'show']);
+        Route::get('/actions/', [App\Http\Controllers\ActionsController::class, 'index']);
+        Route::get('/actions/{code}', [App\Http\Controllers\ActionsController::class, 'show']);
     });
 });
